01690144be5da4e716e0e3572dd907b0e0f48dbc
Mikhail Kucherenko <mihail.k.work@mail.ru>
2023-08-09 21:55:59 +0300

    tipescript-4

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -1,7 +1,16 @@
 import Cart from '../service/Cart';
+import Movie from '../domain/Movie';
 
 test('new card should be empty', () => {
   const cart = new Cart();
 
   expect(cart.items.length).toBe(0);
+});
+
+test('Добавляем фильм в карзину', () => {
+  const cart = new Cart();
+  const movie = new Movie(1009, 'Мстители', 2012, 'США', 'Avengers Assemble!', 'Фантастика, Боевик, Фентази', '137 мин./02:17', 1500);
+  cart.add(movie);
+
+  expect(cart.items.length).toBe(1);
 });
